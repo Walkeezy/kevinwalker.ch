e40de6d88fb1bc377ec16bd4eff99046636ffe1d
Kevin Walker <mail@kevinwalker.ch>
2021-07-27 16:38:44 +0200

    Updated content, added contact links, changed bg gradient

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,24 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head title="Kevin Walker – Webentwickler"></Head>
+      <Head title="Kevin Walker – Frontend engineer"></Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Kevin Walker</h1>
-        <p className={styles.description}>Webdeveloper & digital consultant</p>
+        <p className={styles.description}>Frontend engineer</p>
+        <div className={styles.contact}>
+          <a href="mailto:mail@kevinwalker.ch" target="_blank" rel="noopener noreferrer">
+            mail@kevinwalker.ch
+          </a>
+          &middot;
+          <a
+            href="https://www.linkedin.com/in/kevin-walkeezy/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
       </main>
     </div>
   )
