2998537a85bc5e1a36a65687008ed4c3f411bbb7
Kevin Walker <mail@kevinwalker.ch>
2021-08-25 14:58:43 +0200

    Updated packages, removed next-google-fonts

--- pages/components/head.js
+++ pages/components/head.js
@@ -1,15 +1,20 @@
 import * as React from 'react'
 import NextHead from 'next/head'
-import GoogleFonts from 'next-google-fonts'
 
 export const Head = ({ children, title }) => (
   <React.Fragment>
-    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Karla&family=Rubik&display=swap" />
     <NextHead>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <title>{title}</title>
+      <link rel="shortcut icon" href="/favicon.png" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Karla:wght@400;600&display=swap"
+        rel="stylesheet"
+      />
       {children}
     </NextHead>
   </React.Fragment>
