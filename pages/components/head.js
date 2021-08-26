f1160c5e0e729580c30b6c197b941fc237b685a3
Kevin Walker <mail@kevinwalker.ch>
2021-08-26 14:48:17 +0200

    Add language and meta description

--- pages/components/head.js
+++ pages/components/head.js
@@ -8,6 +8,10 @@ export const Head = ({ children, title }) => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <title>{title}</title>
+      <meta
+        name="description"
+        content="Kevin Walker is a frontend engineer from Switzerland."
+      ></meta>
       <link rel="shortcut icon" href="/favicon.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
